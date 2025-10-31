b65e65ba3609b2770eb95bedcb3041e304ece413
ajdinkarisik <karisikajdins@gmail.com>
2025-10-31 23:43:14 +0100

    Assignment 2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Assignment-2
+Programmet anropar först funktionen fetchMeals()
+Den hämtar sedan data från TheMealDB och gör om svaret till JSON.
+Alla måltider sparas i en varianel som heter "meals".
+Den sorterar måltiderna i ett alfabetiskt ordning.
+Den filtrerar fram alla måltider som tillhör en viss kategori, till exempel "Seafood".
+Räknar hur många måltider som finns i varje kategori med reduce().
+Skriver sedan ut alla resultat i webbläsarens konsol.
+Om något går fel så visas ett meddelande i konsolen med hjlp av "(try catch)".
--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-async function fetchMeals(){
+async function fetchMeals() {
   try {
-    const response= await fetch("https://www.themealbd.com/api/json/v1/1/search.php?s=");
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
     const data = await response.json();
     const meals = data.meals;
-    console.log("Alla måltider:", meals);
 
-    const firstFiveMeals=meals
-      .sort((a, b)=> a.strMeal.localeCompare(b.strMeal))
-      .slice(0, 5)
-      .map(meal=> meal.strMeal);
-
-    console.log("Första 5 måltider (alfabetiskt):", firstFiveMeals);
+    const sortedMeals = meals.sort(function(a, b) {
+      return a.strMeal.localeCompare(b.strMeal);
+    });
+    const mealNames = sortedMeals.map(function(meal) {
+      return meal.strMeal;
+    });
+    const firstFiveNames = mealNames.slice(0, 5);
+    console.log(firstFiveNames);
 
     const chosenCategory = "Seafood";
+    const filteredMeals = meals.filter(function(meal) {
+      return meal.strCategory.toLowerCase() === chosenCategory.toLowerCase();
+    });
+    filteredMeals.forEach(function(meal) {
+      console.log(meal.strMeal + " (" + meal.strCategory + ")");
+    });
 
-    const mealsInCategory = meals.filter(
-      meal => meal.strCategory.toLowerCase() === chosenCategory.toLowerCase()
-    );
-
-    console.log(`Måltider i kategorin ${chosenCategory}:`,
-      mealsInCategory.map(meal => `${meal.strMeal} (${meal.strCategory})`)
-    );
-
-    const categoryCount = meals.reduce ((acc, meal)=> {
-      const category=meal.strCategory;
-      acc[category] = (acc[category] || 0) + 1;
+    const categoryCount = meals.reduce(function(acc, meal) {
+      const category = meal.strCategory;
+      if (!acc[category]) {
+        acc[category] = 0;
+      }
+      acc[category]++;
       return acc;
-    }, {};
+    }, {});
+    console.log(categoryCount);
 
-    console.log("Antal måltider per kategori:", categoryCount);
   } catch (error) {
-    console.error("Fel vid hämtning av data:", error);
+    console.error(error);
   }
 }
+
+fetchMeals();
